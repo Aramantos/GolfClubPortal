26d169c46050d513702cb3783fd63e3cc7e1c116
Aramantos <john.doyle.mail@icloud.com>
2022-02-02 16:13:39 +0000

    update settings.py

--- golf_club_portal/settings.py
+++ golf_club_portal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o@a)qu(g3*zl9wj=zfs9_6^vj+*9qqo)4t$23m9m5=t8st)p-w'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
