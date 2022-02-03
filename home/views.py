99503fe40b0a5757659e053e5356467370feef01
Aramantos <john.doyle.mail@icloud.com>
2022-02-03 19:47:48 +0000

    update notes template, add notes nav and delete functionality

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from notes.models import Note
+
 
 def index(request):
     """ A view to return the index page"""
 
-    return render(request, 'home/index.html')
+    notes = Note.objects.all()
+    urgent = notes.filter(urgent = True)
+
+    context = {
+        'urgent': urgent,
+    }
+
+    return render(request, 'home/index.html', context)
--- notes/views.py
+++ notes/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
-from .models import Note
+from .models import Note, Category
 
 
 @login_required
 def notes(request):
-    """ A view to return the shop page """
+    """ A view to return the notes page """
 
     notes = Note.objects.all()
+    query = None
+    categories = None
+    urgentcheck = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            notes = notes.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'urgent' in request.GET:
+            urgentcheck = request.GET['urgent'].split(',')
+            notes = notes.filter(urgent = True)
+            urgentcheck = Note.objects.filter(urgent = True)
 
     context = {
         'notes': notes,
+        'search_term': query,
     }
 
     return render(request, 'notes/notes.html', context)
