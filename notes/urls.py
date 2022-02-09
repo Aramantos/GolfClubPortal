f8bd17402353a0307d7cd514cc9bf59149a2edd4
Aramantos <john.doyle.mail@icloud.com>
2022-02-09 16:17:32 +0000

    add edit_note to urls.py

--- notes/urls.py
+++ notes/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.notes, name='notes'),
     path('add/', views.add_note, name='add_note'),
+    path('edit/<int:note_id>/', views.edit_note, name='edit_note'),
     path('delete/<int:note_id>/', views.delete_note, name='delete_note'),
 ]
